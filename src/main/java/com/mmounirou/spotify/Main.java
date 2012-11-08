37117a465345ced63d7ef33f368cfbd8defdd76a
mmounirou <mmounirou@mmounirou.com>
2012-11-08 01:16:15 +0100

    build the main class

--- src/main/java/com/mmounirou/spotify/Main.java
+++ src/main/java/com/mmounirou/spotify/Main.java
@@ -1,18 +1,24 @@
 package com.mmounirou.spotify;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Collection;
+import java.util.Properties;
 import java.util.Set;
 
 import javax.annotation.Nullable;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Sets;
+import com.google.common.eventbus.EventBus;
 import com.google.common.io.CharStreams;
 import com.mmounirou.spotify.dao.AlbumDao;
 import com.mmounirou.spotify.dao.ArtistDao;
@@ -25,6 +31,23 @@ import com.mmounirou.spoty4j.core.Artist;
 
 public class Main
 {
+
+	public static class PrefectMatch implements Predicate<Artist>
+	{
+
+		private String strArtist;
+
+		public PrefectMatch(String strArtist)
+		{
+			this.strArtist = strArtist;
+		}
+
+		public boolean apply(@Nullable Artist artist)
+		{
+			return StringUtils.equalsIgnoreCase(strArtist, artist.getName());
+		}
+
+	}
 
 	public static class ToDbArtist implements Function<Artist, Artists>
 	{
@@ -50,47 +73,50 @@ public class Main
 		}
 	}
 
-	private static void checkAlbumRelease(ArtistDao artistDao, AlbumDao albumDao)
-	{
-		ImmutableList<Artists> artists = artistDao.all();
-		Set<Album> allAlbums = Sets.newHashSet();
-
-		// Get all albums for this
-		for ( Artists artist : artists )
-		{
-			Artist spotifyArtist = new Artist(artist.getUri(), artist.getName()).fetch();
-			allAlbums.addAll(spotifyArtist.getAlbums());
-		}
-
-		Collection<Album> insertedAlbums = albumDao.addAlbumsIfNotExists(allAlbums);
-
-		for ( Album album : insertedAlbums )
-		{
-			notifyNewAlbum(album);
-		}
-	}
-
 	private static void createArtists(ArtistDao artistDao) throws IOException
 	{
 		Set<Artist> allArtists = Sets.newHashSet();
-		for ( String strArtist : CharStreams.readLines(new InputStreamReader(Sample.class.getResourceAsStream("/artists.txt"))) )
+		for (String strArtist : CharStreams.readLines(new InputStreamReader(Sample.class.getResourceAsStream("/artists.txt"))))
 		{
-			allArtists.addAll(Search.searchArtist(strArtist));
+			if (StringUtils.isNotBlank(strArtist))
+			{
+				ImmutableList<Artist> spotifyArtist = FluentIterable.from(Search.searchArtist(strArtist)).filter(new PrefectMatch(strArtist)).toImmutableList();
+				allArtists.addAll(spotifyArtist);
+				System.out.println("Fetch Artist : " + strArtist + " : " + Joiner.on(",").join(spotifyArtist));
+			}
 		}
 		artistDao.addArtistIfNotExist(FluentIterable.from(allArtists).transform(new ToDbArtist()).toImmutableList());
 	}
 
-	private static void notifyNewAlbum(Album album)
-	{
-		System.out.println(String.format("New Album : %s : %s : %s", album.getHref(), album.getArtist().getName(), album.getName()));
-	}
-
 	public static void main(String[] args) throws SQLException, IOException
 	{
-		Connection connection = DBUtils.initDataBase(false);
+
+		boolean dropDataBase = false;
+		EventBus eventBus = new EventBus();
+
+		if(args.length == 1)
+		{
+			Properties properties = new Properties();
+			properties.load(new FileInputStream(args[0]));
+			MailAlbumListener mailListener = new MailAlbumListener(properties);
+			eventBus.register(mailListener);
+		}
+		
+		Object eventListener = new LogAlbumListener();
+		eventBus.register(eventListener);
+		
+		long begin = System.currentTimeMillis();
+		AlbumReleaseChecker scanner = new AlbumReleaseChecker(eventBus);
+
+		Connection connection = DBUtils.initDataBase(dropDataBase);
 		ArtistDao artistDao = new ArtistDao(connection);
 		AlbumDao albumDao = new AlbumDao(connection);
 		createArtists(artistDao);
-		checkAlbumRelease(artistDao, albumDao);
+		
+		scanner.checkAlbumRelease(artistDao, albumDao);
+
+		long end = System.currentTimeMillis();
+
+		System.out.println("Export take " + (end - begin) / 1000 + " seconds");
 	}
 }
