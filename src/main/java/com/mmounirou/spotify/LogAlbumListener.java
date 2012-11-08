e89d21571d2793f7b2ef6bf592bad4e4924ab815
mmounirou <mmounirou@mmounirou.com>
2012-11-08 01:16:43 +0100

    implements a simple console log notifier

---
+++ src/main/java/com/mmounirou/spotify/LogAlbumListener.java
@@ -0,0 +1,13 @@
+package com.mmounirou.spotify;
+
+import com.google.common.eventbus.Subscribe;
+import com.mmounirou.spotify.AlbumReleaseChecker.NewAlbumEvent;
+
+public class LogAlbumListener
+{
+	@Subscribe
+	public void receiveNewAlbum(NewAlbumEvent newAlbumEvent)
+	{
+		System.out.println("New Album = " + newAlbumEvent.getAlbum());
+	}
+}
