44f01b28b16d7e8ee534429dac78a11d46844db0
mmounirou <mmounirou@mmounirou.com>
2012-11-08 11:34:01 +0100

    implements the reset command

--- src/main/java/com/mmounirou/spotify/commands/ResetCommand.java
+++ src/main/java/com/mmounirou/spotify/commands/ResetCommand.java
@@ -1,13 +1,11 @@
 package com.mmounirou.spotify.commands;
 
-
 public class ResetCommand implements Command
 {
 
 	public void run() throws CommandException
 	{
-		// TODO Auto-generated method stub
-
+		new DropCommand(true).run();
 	}
 
 }
