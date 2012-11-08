a67952f96a8604f786584d0c232e0f98864e9923
mmounirou <mmounirou@mmounirou.com>
2012-11-08 01:14:34 +0100

    create artist in batch mode

--- src/main/java/com/mmounirou/spotify/dao/ArtistDao.java
+++ src/main/java/com/mmounirou/spotify/dao/ArtistDao.java
@@ -4,11 +4,15 @@ import java.sql.Connection;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
+import javax.annotation.Nullable;
+
+import com.google.common.base.Function;
 import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.mmounirou.spotify.commons.util.ListUtils;
 import com.mmounirou.spotify.datamodel.Artists;
 import com.mmounirou.spotify.datamodel.query.QArtists;
 import com.mysema.query.sql.SQLQueryImpl;
@@ -18,6 +22,7 @@ import com.mysema.query.sql.dml.SQLInsertClause;
 public class ArtistDao
 {
 
+	private static final int MAX_ELMT_BY_QUERY = 500;
 	private Connection m_connection;
 
 	public ArtistDao(Connection connection)
@@ -34,12 +39,12 @@ public class ArtistDao
 
 	public void addArtists(Collection<Artists> artists)
 	{
-		if ( !artists.isEmpty() )
+		if (!artists.isEmpty())
 		{
 			long begin = System.currentTimeMillis();
 			QArtists tartists = QArtists.tArtists;
 			SQLInsertClause insertClause = new SQLInsertClause(m_connection, new SQLiteTemplates(), tartists);
-			for ( Artists artist : artists )
+			for (Artists artist : artists)
 			{
 				//@formatter:off
 				insertClause.set(tartists.uri, artist.getUri())
@@ -63,26 +68,36 @@ public class ArtistDao
 		return ImmutableList.copyOf(query.from(tartists).list(tartists));
 	}
 
-	public List<String> exist(Set<String> artistHrefs)
+	public List<String> exist(List<String> artistHrefs)
 	{
-		System.out.println(artistHrefs.size());
-		SQLQueryImpl query = new SQLQueryImpl(m_connection, new SQLiteTemplates());
-		QArtists tartists = QArtists.tArtists;
-		return query.from(tartists).where(tartists.uri.in(artistHrefs)).listDistinct(tartists.uri);
+		Function<List<String>, Iterable<String>> filterExistingArtists = new Function<List<String>, Iterable<String>>()
+		{
+
+			@Nullable
+			public Iterable<String> apply(@Nullable List<String> splittedArtist)
+			{
+				SQLQueryImpl query = new SQLQueryImpl(m_connection, new SQLiteTemplates());
+				QArtists tartists = QArtists.tArtists;
+				return query.from(tartists).where(tartists.uri.in(splittedArtist)).listDistinct(tartists.uri);
+			}
+		};
+		return FluentIterable.from(ListUtils.split(MAX_ELMT_BY_QUERY, artistHrefs)).transformAndConcat(filterExistingArtists).toImmutableList();
+
 	}
 
 	public Collection<Artists> addArtistIfNotExist(Collection<Artists> artists)
 	{
 		Map<String, Artists> artistsById = Maps.newHashMap();
-		for ( Artists artist : artists )
+		for (Artists artist : artists)
 		{
 			artistsById.put(artist.getUri(), artist);
 		}
 
-		List<String> existingArtists = exist(artistsById.keySet());
+		List<String> existingArtists = exist(Lists.newArrayList(artistsById.keySet()));
 		artistsById.keySet().removeAll(existingArtists);
 		Collection<Artists> values = artistsById.values();
 		addArtists(values);
+
 		return values;
 	}
 
